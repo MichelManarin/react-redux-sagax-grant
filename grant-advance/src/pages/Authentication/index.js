4c6bafd5b327b8a546cbb467c4a90d93687acea3
Michel Manarin <michelmanarin@gmail.com>
2024-01-25 21:30:42 -0300

    refactor: improve ux & ui authentication page

--- grant-advance/src/pages/Authentication/index.js
+++ grant-advance/src/pages/Authentication/index.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { TextField } from "@mui/material";
-
 import { Grid, Paper, Box, Typography, Button, Divider } from "@mui/material";
-
 import { loginRequest } from "../../store/actions";
 import { Form } from "@unform/web";
+
 export default function AuthenticationPage() {
   const styleError = { color: "red" };
   const dispatch = useDispatch();
@@ -44,66 +43,63 @@ export default function AuthenticationPage() {
         spacing={2}
         direction="column"
       >
-        <Grid item xs={12}>
+        <Grid>
           <Paper elevation={0} variant="elevation">
             <Box className="box form-login ">
-              <Grid container spacing={2} style={{ margin: "auto" }}>
-                <Grid item xs={12}>
-                  <Grid container spacing={0} direction="column-reverse">
-                    <Grid item>
-                      <Typography variant="h6" gutterBottom>
-                        🚀 Welcome back
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                  <Form onSubmit={handleSubmit}>
-                    <TextField
-                      autoComplete={"off"}
-                      variant="outlined"
-                      margin="normal"
-                      fullWidth
-                      id="login"
-                      label="Login"
-                      name="login"
-                      value={form.login}
-                      onChange={(e) =>
-                        setForm({
-                          ...form,
-                          login: e.target.value,
-                        })
-                      }
-                    />
-                    <TextField
-                      autoComplete={"off"}
-                      variant="outlined"
-                      margin="normal"
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      value={form.password}
-                      onChange={(e) =>
-                        setForm({
-                          ...form,
-                          password: e.target.value,
-                        })
-                      }
-                    />
-                    {error && <p style={styleError}>{error}</p>}
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      style={{ marginTop: 16 }}
-                      onClick={handleSubmit}
-                    >
-                      Sign In
-                    </Button>
-                  </Form>
-                </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6" gutterBottom>
+                  🚀 Welcome back
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Form onSubmit={handleSubmit}>
+                  <TextField
+                    size="small"
+                    autoComplete={"off"}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    id="login"
+                    label="Login"
+                    name="login"
+                    value={form.login}
+                    onChange={(e) =>
+                      setForm({
+                        ...form,
+                        login: e.target.value,
+                      })
+                    }
+                  />
+                  <TextField
+                    size="small"
+                    autoComplete={"off"}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={form.password}
+                    onChange={(e) =>
+                      setForm({
+                        ...form,
+                        password: e.target.value,
+                      })
+                    }
+                  />
+                  {error && <p style={styleError}>{error}</p>}
+                  <Button
+                    size="large"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    style={{ marginTop: 16 }}
+                    onClick={handleSubmit}
+                  >
+                    Sign In
+                  </Button>
+                </Form>
               </Grid>
             </Box>
           </Paper>
@@ -112,9 +108,7 @@ export default function AuthenticationPage() {
           <Divider />
         </Grid>
         <Grid item xs={12}>
-          <div className="css-9jay18">
-            <Typography variant="subtitle2">GrantAdvance Test</Typography>
-          </div>
+          <Typography variant="subtitle2">GrantAdvance Test</Typography>
         </Grid>
       </Grid>
     </div>
